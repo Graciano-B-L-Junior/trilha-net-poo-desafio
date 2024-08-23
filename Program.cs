c5a2fbef35e86d21a622163adc1d9948ec67de92
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2024-08-23 11:22:26 -0300

    projeto finalizado utilizando .net 8

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("9999-999","Tijolinho","imei-nokia",206);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("snake");
+
+Iphone iphone = new Iphone("9999-999","Tijolinho","imei-iphone",1024);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Angry birds");
